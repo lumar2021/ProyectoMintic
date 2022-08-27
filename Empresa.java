2c4b04f5d90457b91ef45870585bc2f3b19e8959
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2022-08-26 22:05:09 -0500

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -1,1 +1,66 @@
+import java.util.ArrayList;
 
+public class Empresa
+{
+  private String nombre;
+  private String direccion;
+  private String telefono;
+  private String NIT;
+  private ArrayList<Empleado> empleados;
+
+  public Empresa(){}
+
+  public Empresa(String nombre, String direccion, String telefono, String NIT)
+  {
+    this.nombre= nombre;
+    this.direccion=direccion;
+    this.telefono=telefono;
+    this.NIT=NIT;
+    this.empleados = new ArrayList<Empleado>();
+  }
+
+  public void setNombre(String nombre)
+  {
+    this.nombre=nombre;
+  }
+
+  public void setDireccion(String direccion)
+  {
+    this.direccion=direccion;
+  }
+
+  public void setTelefono(String telefono)
+  {
+    this.telefono=telefono;
+  }
+
+  public void setNIT(String NIT)
+  {
+    this.NIT=NIT;
+  }
+
+  public String getNombre() {
+    return nombre;  
+  }
+  
+  public String getDireccion() {
+    return direccion;  
+  }
+  
+  public String getTelefono() {
+    return telefono;  
+  }
+  
+  public String getNIT() {
+    return NIT;
+  }
+
+  public void setEmpleados(ArrayList<Empleado> empleados)
+  {
+    this.empleados = empleados;
+  }
+
+  public ArrayList<Empleado> getEmpleados() {
+    return this.empleados;  
+  }
+}
