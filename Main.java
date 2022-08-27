6123ea8ae885eca38f5a766dc85b0816d20715b3
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2022-08-26 22:06:22 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    Empresa empresa1 = new Empresa("Mc donals", "Calle 8", "3002020", "1010");
+    System.out.println("Nombre empresa: "+empresa1.getNombre());
+
+    Empleado empleado1 = new Empleado("Sara", "s@mcdonals.com", "administrador", "CC", 1010101010);
+    empleado1.setEmpresa(empresa1);
+    System.out.println("Nombre del empleado: "+empleado1.getNombre());
+    System.out.println("Nombre de la empresa del empleado: "+empleado1.getEmpresa().getNombre());
+
+    MovimientoDinero movimiento1 = new MovimientoDinero(200, "Compre gaseosas", "egreso");
+    movimiento1.setEmpleado(empleado1);
+    System.out.println("Monto del movimiento: "+movimiento1.getMonto());
+    System.out.println("Nombre del empleado que hizo el movimiento: "+movimiento1.getEmpleado().getNombre());
+
+  }
+}
