cd5b5a1c77889e30068d3f65fdbbdd4d68f57289
julianapg <julianapg92@gmail.com>
2022-09-19 19:26:31 -0500

    Se pueden crear empresas y empleados

--- project/src/main/java/com/mintic/project/controllers/ControladorEmpleado.java
+++ project/src/main/java/com/mintic/project/controllers/ControladorEmpleado.java
@@ -1,13 +1,14 @@
 package com.mintic.project.controllers;
 
 
+import com.mintic.project.models.Empleado;
+import com.mintic.project.models.Empresa;
 import com.mintic.project.services.ServicioEmpleado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
@@ -19,39 +20,15 @@ public class ControladorEmpleado {
 
     @GetMapping("/empleados")
     public String consultarEmpleados(Model model) {
+        Empleado empleado = new Empleado();
+        model.addAttribute("empleado", empleado);
         model.addAttribute("empleados", servicioEmpleado.consultarTodas());
         return "empleados";
     }
 
-    /*
-
-    @CrossOrigin
-    @GetMapping("/empleados/{id}")
-    public String consultarUnEmpleado(@PathVariable int id)
-    {
-        return servicioEmpleado.consultarUna(id);
-    }
-
-    @CrossOrigin
-    @DeleteMapping("/empleados/{id}")
-    public String eliminarUnEmpleado(@PathVariable int id){
-        servicioEmpleado.eliminar(id);
-        return "Empleado con id: "+id+" borrado";
-    }
-
-    @CrossOrigin
     @PostMapping("/empleados")
-    public String crearEmpleado(@RequestBody Empleado empleado){
+    public RedirectView crearEmpleado(@ModelAttribute("empleado") Empleado empleado){
         servicioEmpleado.crear(empleado);
-        return "Empleado creado";
+        return new RedirectView("/empleados");
     }
-
-    @CrossOrigin
-    @PatchMapping("/empleados/{id}")
-    public String actualizarUnEmpleado(@PathVariable int id, @RequestBody Empleado empleado){
-        servicioEmpleado.actualizar(id,empleado);
-        return "Empleado actualizada";
-    }
-
-     */
 }
--- project/src/main/java/com/mintic/project/controllers/ControladorEmpresas.java
+++ project/src/main/java/com/mintic/project/controllers/ControladorEmpresas.java
@@ -1,10 +1,12 @@
 package com.mintic.project.controllers;
 
+import com.mintic.project.models.Empresa;
 import com.mintic.project.services.ServicioEmpresa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ControladorEmpresas {
@@ -15,36 +17,15 @@ public class ControladorEmpresas {
 
     @GetMapping("/empresas")
     public String consultarEmpresas(Model model){
+        Empresa empresa = new Empresa();
+        model.addAttribute("empresa", empresa);
         model.addAttribute("empresas", servicioEmpresa.consultarTodas());
         return "empresas";
     }
-/*
-    @CrossOrigin
-    @GetMapping("/empresas/{id}")
-    public String consultarUnaEmpresa(@PathVariable int id){
-        return servicioEmpresa.consultarUna(id);
-    }
 
-    @CrossOrigin
-    @DeleteMapping("/empresas/{id}")
-    public String eliminarUnaEmpresa(@PathVariable int id){
-        servicioEmpresa.eliminar(id);
-        return "Empresa con id: "+id+" borrada";
-    }
-
-    @CrossOrigin
     @PostMapping("/empresas")
-    public String crearEmpresas(@RequestBody Empresa empresa){
+    public RedirectView crearEmpresa(@ModelAttribute("empresa") Empresa empresa){
         servicioEmpresa.crear(empresa);
-        return "Empresa creada";
+        return new RedirectView("/empresas");
     }
-
-    @CrossOrigin
-    @PatchMapping("/empresas/{id}")
-    public String actualizarUnaEmpresa(@PathVariable int id, @RequestBody Empresa empresa){
-        servicioEmpresa.actualizar(id, empresa);
-        return "Empresa actualizada";
-    }
-
- */
 }
