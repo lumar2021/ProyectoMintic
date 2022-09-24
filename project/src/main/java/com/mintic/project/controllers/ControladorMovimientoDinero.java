f8a770ee8edd08d8e15bc5be983e15c022d2a308
= <=>
2022-09-23 20:52:02 -0500

    Template movimiento dinero

--- project/src/main/java/com/mintic/project/controllers/ControladorMovimientoDinero.java
+++ project/src/main/java/com/mintic/project/controllers/ControladorMovimientoDinero.java
@@ -5,46 +5,29 @@ import com.mintic.project.models.Empresa;
 import com.mintic.project.models.MovimientoDinero;
 import com.mintic.project.services.ServicioMovimientoDinero;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 
-@RestController
+@Controller
 public class ControladorMovimientoDinero {
 
     @Autowired
     private ServicioMovimientoDinero servicioMovimientoDinero;
-
-    @CrossOrigin
-    @GetMapping("/empresas/{id}/movimientos")
-    public String consultarMovimientos(@PathVariable int id) {
-        return servicioMovimientoDinero.consultarTodosPorIdEmpresa(id);
+    @GetMapping("/movimientos")
+    public String consultarMovimientos(Model model){
+        MovimientoDinero movimiento= new MovimientoDinero();
+        model.addAttribute("movimiento", movimiento);
+        model.addAttribute("movimientos", servicioMovimientoDinero.consultarTodas());
+        return "movimientos";
     }
 
-    @CrossOrigin
-    @GetMapping("/empresas/{id}/movimientos/{id2}")
-    public String consultarUnMov(@PathVariable int id,@PathVariable int id2){
-        return servicioMovimientoDinero.consultarUno(id2);
-    }
-
-    @CrossOrigin
-    @DeleteMapping("/empresas/{id}/movimientos/{id2}")
-    public String eliminarUnMov(@PathVariable int id,@PathVariable int id2){
-        servicioMovimientoDinero.eliminar(id2);
-        return "Movimiento con id: "+id2+" borrado";
-    }
-
-    @CrossOrigin
-    @PostMapping("/empresas/{id}/movimientos")
-    public String crearUnMov(@PathVariable int id, @RequestBody MovimientoDinero movimientoDinero){
-        servicioMovimientoDinero.crear(id, movimientoDinero);
-        return "Movimiento creado";
-    }
-
-    @CrossOrigin
-    @PatchMapping("/empresas/{id}/movimientos/{id2}")
-    public String actualizarUnMov(@PathVariable int id, @PathVariable int id2, @RequestBody MovimientoDinero movimientoDinero){
-        servicioMovimientoDinero.actualizar(id, id2, movimientoDinero);
-        return "Movimiento actualizado";
+    @PostMapping("/movimientos")
+    public RedirectView crearEmpresa(@ModelAttribute("movimiento") MovimientoDinero movimiento){
+        servicioMovimientoDinero.crear(movimiento);
+        return new RedirectView("/movimientos");
     }
 
 }
